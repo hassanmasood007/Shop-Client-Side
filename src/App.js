ff6063b4a084b3ba8a7716e38d60f4b57f06a5bc
Hassan Masood <hassanmasood2002@gmail.com>
2022-04-16 14:33:21 +0500

    Routes Added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-// import Cart from "./pages/Cart";
+import Cart from "./pages/Cart";
 import Home from "./pages/Home";
-// import Login from "./pages/Login";
-// import Product from "./pages/Product";
-// import ProductList from "./pages/ProductList";
-// import Register from "./pages/Resgister";
+import Login from "./pages/Login";
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Resgister";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Home />
-      {/* <ProductList /> */}
-      {/* <Product /> */}
-      {/* <Register /> */}
-      {/* <Login /> */}
-      {/* <Cart /> */}
-    </>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/products/:category">
+          <ProductList />
+        </Route>
+        <Route path="/product/:id">
+          <Product />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
